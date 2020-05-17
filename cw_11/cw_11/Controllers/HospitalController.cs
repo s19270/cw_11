f58aed0a5b1dd837e635ee631999b49f42ce62b5
s19270 <61821772+s19270@users.noreply.github.com>
2020-05-17 14:26:01 +0200

    commit

--- cw_11/cw_11/Controllers/HospitalController.cs
+++ cw_11/cw_11/Controllers/HospitalController.cs
@@ -22,5 +22,30 @@ namespace cw_11.Controllers
         {
             return Ok(_context.Doctor.ToList());
         }
+        [HttpPost]
+        public IActionResult AddDoctors(Doctor doc)
+        {
+            _context.Doctor.Add(doc);
+            _context.SaveChanges();
+            return Ok("Dodano lekarza");
+        }
+        [HttpPut]
+        public IActionResult UpdateDoctors(Doctor doc)
+        {
+            var d = _context.Doctor.Where(a => a.IdDoctor == doc.IdDoctor).First();
+            d.FirstName = doc.FirstName;
+            d.LastName = doc.LastName;
+            d.Email = doc.Email;
+            _context.SaveChanges();
+            return Ok("Zmieniona dane lekarza");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteDoctors(string id)
+        {
+            var d = _context.Doctor.Where(a => a.IdDoctor == int.Parse(id)).First();
+            _context.Doctor.Remove(d);
+            _context.SaveChanges();
+            return Ok("Usunieto lekarza");
+        }
     }
 }
